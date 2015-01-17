20128909e7ed685b4d562e9b3e03d925723fb1e3
veronicawharton <veronica.wharton@yahoo.com>
2015-01-17 15:30:43 -0500

    Painting on all 6 sides

--- pennapps15w/Assets/Cardboard/DemoScene/Paint.cs
+++ pennapps15w/Assets/Cardboard/DemoScene/Paint.cs
@@ -5,21 +5,22 @@ using System.Collections;
 public class Paint : MonoBehaviour {
 	private CardboardHead head;
 	private Vector3 startingPosition;
-	public Texture2D texture; 
-	public Texture2D[] textures; 
+	public Texture2D texture, texture1, texture2, texture3, texture4, texture5; 
+	//public Texture2D[] textures = new Texture2D[6]; 
 	private GameObject[] planes;
 	private int NUM_PLANES = 6; 
 	private MeshCollider meshCollider; 
+	private Color penColor = Color.black; 
 
 	void Start() {
 		planes = new GameObject[NUM_PLANES];
-		textures = new Texture2D[NUM_PLANES]; 
+		//textures = new Texture2D[NUM_PLANES]; 
 
 		// Create plane canvas programmatically.
 		// Create 6 textures.
 		for (int i = 0; i < NUM_PLANES; i++) {
 			planes[i] = GameObject.CreatePrimitive(PrimitiveType.Plane);
-			textures[i] = Texture2D.blackTexture; 
+			//textures[i] = Texture2D.blackTexture; 
 		}
 		// Plane in front of camera.
 		planes[0].transform.position = new Vector3(0, 5.6F, 5F);
@@ -44,9 +45,6 @@ public class Paint : MonoBehaviour {
 		planes[5].transform.position = new Vector3(0, 5.6F, 0);
 		planes[5].transform.Rotate(0, 0, 180);
 		meshCollider = planes[5].AddComponent("MeshCollider") as MeshCollider;
-		/*planes[5].transform.position = new Vector3(5F, 0, -5F);
-		planes[5].transform.Rotate(0, 90, 0);
-		meshCollider = planes[5].AddComponent("MeshCollider") as MeshCollider;*/
 
 		head = Camera.main.GetComponent<StereoController>().Head;
 		startingPosition = transform.localPosition;
@@ -55,11 +53,18 @@ public class Paint : MonoBehaviour {
 		
 		// Apply texture to plane.
 		planes[0].renderer.material.mainTexture = texture;
-		for (int i = 1; i < NUM_PLANES; i++) {
-			planes[i].renderer.material.mainTexture = textures[i];
-		}
+		planes[1].renderer.material.mainTexture = texture1;
+		planes[2].renderer.material.mainTexture = texture2;
+		planes[3].renderer.material.mainTexture = texture3;
+		planes[4].renderer.material.mainTexture = texture4;
+		planes[5].renderer.material.mainTexture = texture5;
 
 		texture.Apply();
+		texture1.Apply();
+		texture2.Apply();
+		texture3.Apply();
+		texture4.Apply();
+		texture5.Apply();
 
 		// Save file image.
 		byte[] image = texture.EncodeToJPG();
@@ -81,9 +86,15 @@ public class Paint : MonoBehaviour {
 			Vector2 pixelUV = hit.textureCoord;
 			pixelUV.x *= texture.width;
 			pixelUV.y *= texture.height;
-			Circle(texture, (int)pixelUV.x, (int)pixelUV.y, 5, Color.blue);
+			Circle(texture, (int)pixelUV.x, (int)pixelUV.y, 5, penColor);
 			texture.Apply();
 		}
+
+		DetectLookAt(1, texture1); 
+		DetectLookAt(2, texture2); 
+		DetectLookAt(3, texture3); 
+		DetectLookAt(4, texture4); 
+		DetectLookAt(5, texture5); 
 
 		/*RaycastHit hit;
 		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);*/
@@ -95,6 +106,28 @@ public class Paint : MonoBehaviour {
 			float distance = 2 * Random.value + 1.5f;
 			transform.localPosition = direction * distance;
 		}*/
+	}
+
+	void DetectLookAt(int index, Texture2D tex) {
+		RaycastHit hit;
+		bool isLookedAt = planes[index].GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		// If the plane is being looked out, determine exactly which pixel on the texture is being looked at.
+		if (isLookedAt) {
+			Debug.Log ("Is looked at!"); 
+			Renderer renderer = hit.collider.renderer;
+			MeshCollider meshCollider = hit.collider as MeshCollider;
+			if (renderer == null || renderer.sharedMaterial == null || renderer.sharedMaterial.mainTexture == null || meshCollider == null) {
+				Debug.Log ("Not working");
+				return;
+			}
+			
+			// "Paint" the pixel.
+			Vector2 pixelUV = hit.textureCoord;
+			pixelUV.x *= tex.width;
+			pixelUV.y *= tex.height;
+			Circle(tex, (int)pixelUV.x, (int)pixelUV.y, 5, penColor);
+			tex.Apply();
+		}
 	}
 	
 	void OnGUI() {
