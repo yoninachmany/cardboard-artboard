e59221007cac761270e097d9a6fad602a0f3fa7f
Yoni Nachmany <nachmany@seas.upenn.edu>
2015-01-17 22:11:13 -0500

    Split functions

--- pennapps15w/Assets/Cardboard/DemoScene/Teleport.cs
+++ pennapps15w/Assets/Cardboard/DemoScene/Teleport.cs
@@ -26,16 +26,10 @@ public class Teleport : MonoBehaviour {
 	public AudioClip sound;
 	private AudioSource source;
 
-	public bool isStill = false;
 	float stillTimeLeft = 2.0f;
 	public Vector3 lastVector;
 	public Vector3 currVector;
 	float thresh = .01f;
-
-//  void Awake() {
-//		source = GetComponent<AudioSource> ();
-//		Debug.Log("HELLO?!");
-//  }
 
   void Start() {
 	source = GetComponent<AudioSource> ();
@@ -47,76 +41,79 @@ public class Teleport : MonoBehaviour {
 	currVector = head.Gaze.direction;
   }	
 
-  void Update() {
-    RaycastHit hit;
-	lastVector = currVector;
-	currVector = head.Gaze.direction;
-//	Debug.Log(Vector3.Distance(lastVector,currVector));
+  bool isStill() {
 	if (Vector3.Distance(currVector, lastVector) < thresh) {
 		if (stillTimeLeft > 0) {
 			stillTimeLeft -= Time.deltaTime;
-//			Debug.Log("MORE STILL TIME!");
 		}
 		if (stillTimeLeft < 0) {
-			isStill = true;
-			Debug.Log(stillTimeLeft);
-			Debug.Log("isStill!");
-			audio.Play();
-//			source.PlayOneShot(sound, 1F);
-//			GameObject cyl = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-//			//		plane.transform.Translate(0,0,8);
-//			//		plane.transform.Rotate (270, 0, 0);
-//			cyl.transform.parent = transform;
-//			cyl.transform.localPosition = new Vector3(0f, 0f, 2.5f);
-//			cyl.transform.Rotate(90, 0, 0);
-//			cyl.transform.localScale = new Vector3(0.1f, .0000000007f, 0.1f);
-//			cyl.renderer.material.color = Color.black;
+			stillTimeLeft = 2.0f;
+			return true;
 		}
+		return false;
 	}
-	else {
-//		Debug.Log("START AGAIN!");
-		stillTimeLeft = 2.0f;
-		isStill = false;
+	stillTimeLeft = 2.0f;
+	return false;
+  }
+
+  void Update() {
+	lastVector = currVector;
+	currVector = head.Gaze.direction;
+	if (isStill()) {
+		audio.Play();
+		Debug.Log("isStill!");
 	}
 
-	//Debug.Log (GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity));
-    bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
-	if (isLookedAt) {
-//		Debug.Log("isLookedAt");
-//		Debug.Log(timeLeft);
-		if (timeLeft > 0) {
-			timeLeft -= Time.deltaTime;
-		}
-		if (timeLeft < 0) {
-//			Debug.Log("Color selected!");
-			GetComponent<Renderer>().material.color = Color.blue;
-		}
-	}
-	if (!isLookedAt) {
-
-//		Debug.Log("not isLookedAt");
-		timeLeft = 2.0f;
-	}	
-	
-//    GetComponent<Renderer>().material.color = isLookedAt ? Color.white : Color.grey;
-    if (Cardboard.SDK.CardboardTriggered && isLookedAt) {
-      // Teleport randomly.
-      Vector3 direction = Random.onUnitSphere;
-      direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-      float distance = 2 * Random.value + 1.5f;
-      transform.localPosition = direction * distance;
-    }
+//    RaycastHit hit;
+//
+//    if (Cardboard.SDK.CardboardTriggered && isLookedAt) {
+//      // Teleport randomly.
+//      Vector3 direction = Random.onUnitSphere;
+//      direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
+//      float distance = 2 * Random.value + 1.5f;
+//      transform.localPosition = direction * distance;
   }
 	
   void OnGUI() {
     if (!CardboardGUI.OKToDraw(this)) {
       return;
     }
+  }
+}
 //    if (GUI.Button(new Rect(50, 50, 200, 50), "Reset")) {
 //      transform.localPosition = startingPosition;
 //    }
 //	var guiTime = Time.time - startTime;
 //	//var seconds : int = guiTime % 60;
 //		Debug.Log(guiTime);
-  }
-}
+
+			//	//Debug.Log (GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity));
+			//    bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+			//	if (isLookedAt) {
+			////		Debug.Log("isLookedAt");
+			////		Debug.Log(timeLeft);
+			//		if (timeLeft > 0) {
+			//			timeLeft -= Time.deltaTime;
+			//		}
+			//		if (timeLeft < 0) {
+			////			Debug.Log("Color selected!");
+			//			GetComponent<Renderer>().material.color = Color.blue;
+			//		}
+			//	}
+			//	if (!isLookedAt) {
+			//
+			////		Debug.Log("not isLookedAt");
+			//		timeLeft = 2.0f;
+			//	}	
+			
+			//    GetComponent<Renderer>().material.color = isLookedAt ? Color.white : Color.grey;
+	
+	//			source.PlayOneShot(sound, 1F);
+	//			GameObject cyl = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+	//			//		plane.transform.Translate(0,0,8);
+	//			//		plane.transform.Rotate (270, 0, 0);
+	//			cyl.transform.parent = transform;
+	//			cyl.transform.localPosition = new Vector3(0f, 0f, 2.5f);
+	//			cyl.transform.Rotate(90, 0, 0);
+	//			cyl.transform.localScale = new Vector3(0.1f, .0000000007f, 0.1f);
+	//			cyl.renderer.material.color = Color.black;	
